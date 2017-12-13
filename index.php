5372151e99e1dba75017ad9f3c071cd7174e30fc
Champaloux Benjamin <tiitanyum@gmail.com>
2017-12-13 14:29:28 +0100

    center the p

--- index.php
+++ index.php
@@ -3,7 +3,17 @@
 <head>
 	<title>hello world</title>
 </head>
+<style type="text/css">
+	p{
+		position: absolute;
+		top: 40%;
+		width: 100%;
+		text-align: center;
+		font-family: helvetica;
+		
+	}
+</style>
 <body>
-
+	<p>hi</p>
 </body>
 </html>
